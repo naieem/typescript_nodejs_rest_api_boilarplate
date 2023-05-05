7f3e8052f9fc28691c608ef21c19fb37ff27e298
naieem mahmud supto <naieemsupto@gmail.com>
2023-05-05 03:26:51 +0000

    mongodb config updated

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 let appConfig = {
-    dbConnectionString:"mongodb://localhost/test",
-    serverListeningPort:3005
+    dbConnectionString:"mongodb+srv://naieem:1qazZAQ!@naieem.1jjyciu.mongodb.net/test",
+    serverListeningPort:3005,
+    UserSalt: 'hello world'
 }
 export default appConfig;
